6770f470c2dd01dea69b869d9d2a49e9649c0a58
Saumya Chavan <saumyachavan21@gmail.com>
2026-01-21 00:05:48 +0530

    chore: flatten project structure

--- password_generater/src/App.jsx
+++ password_generater/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
 }*/
 return (
   <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-    <h1 className='text-yellow  text-center my-3'>Password Generator</h1>
+    <h1 className='text-yellow  rmdir password_generatertext-center my-3'>Password Generator</h1>
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
       <input
         type="text"
