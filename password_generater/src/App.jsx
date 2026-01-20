2e2544e7f5f025c67ff000c2fac38f2b1a05e44d
Saumya Chavan <saumyachavan21@gmail.com>
2026-01-20 23:57:02 +0530

    react project

---
+++ password_generater/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState,useCallback } from 'react'
+import React from 'react'
+import './index.css'
+function App() {
+  const [length, setlength] = useState(8)
+  const [numberAllowed,setNumber]=useState(false)
+  const [characterAllowed,setCharacter]=useState(false)
+  const [password,setPassword]=useState("")
+
+  const passwordGenerator=useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numberAllowed) str+="0123456789"
+    if (characterAllowed) str+="!@#$%^&*()_+{}[]~`"
+
+
+  for(i=1;i<=array.length;i++){
+    let chara=Math.floor(Math.random()*str.length+1)
+    pass=str.charAt(chara)
+  }
+
+  setPassword(pass)
+    },
+    [length,numberAllowed,characterAllowed,setPassword])
+
+  /*return (
+    <>
+      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-40 my-8 text-white bg-gray'>test</div>
+      
+    </>
+  );
+}*/
+return (
+  <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <h1 className='text-yellow  text-center my-3'>Password Generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+      <input
+        type="text"
+        value={password}
+        className="outline-none w-full py-1 px-3 bg-white text-black"
+        placeholder="Password"
+        readOnly
+      />
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      {/* Slider and checkboxes will go here */}
+      <p>Test Content</p>
+    </div>
+  </div>
+);
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export const content = [
+  "./password_generater/src/**/*.{js,jsx,ts,tsx}",
+  "./password_generater/index.html",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
